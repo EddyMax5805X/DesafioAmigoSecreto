a33a83e149e16d704d3a449dbb4516bf75215468
Ednilson Paulo <ednilsonpaulosambo@gmail.com>
2025-02-03 23:08:06 +0200

    Adicao de funcoes para o app.js, de forma que ele possa rodar o sorteador

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let nomes = [];
+function adicionarAmigo() {
+    let nome = document.querySelector('#amigo');
+    if (nome.value == '') {
+        window.alert('Por favor, insira um nome!');
+    } else {
+       nomes.push(nome.value);
+        console.log(nomes);
+        console.log(nome.value);
+        nome.value = '';
+        nome.focus(); 
+    }
+}
+addEventListener('click', atualizarLista);
+function atualizarLista() {
+    let lista = document.querySelector('#listaAmigos');
+    lista.innerHTML = '';
+    for (let i = 0; i < nomes.length; i++) {
+        lista.innerHTML += `<li>${nomes[i]}</li>`;
+    }
+}
+function sortearAmigo() {
+    if (nomes.length < 2) {
+        alert('Por favor, adicione pelo menos 2 amigos a lista antes de sortear!')
+    } else {
+        let sorteado = Math.floor(Math.random() * nomes.length);
+        console.log(sorteado);
+        let amigoSecreto = document.querySelector('#resultado');
+        amigoSecreto.innerHTML = 'O seu amigo secreto é ' + nomes[sorteado]
+    }
+    
+}
+    
